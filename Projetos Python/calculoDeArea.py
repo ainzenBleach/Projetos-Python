c2df5047ba7cbb8b190cfaffce3bdab8ff932871
ainzenBleach <samuel.barros@sptech.school>
2025-03-29 19:04:40 -0300

    Atualização do read e atividade e treino em python

--- Projetos Python/calculoDeArea.py
+++
@@ -1,6 +0,0 @@
-# uso de operadores aritimedicos para calcular
-pi = float(3.14)
-perimetro = float(input("Digite o perimetro em cm: "))
-area = pi * perimetro
-# utilizndo o %. e especificando o tip de valor (float(f), int(i) e char(s))
-print("A area será de %.1f cm" %area) 
--- Projetos Python/loop (while).py
+++
@@ -1,6 +0,0 @@
-#Criação de loop com while, depois do comando while colocamos a condição para parar os codigos dentro do while
-x=1
-while x<=15:
-    print(x)
-    x=x+1
-
